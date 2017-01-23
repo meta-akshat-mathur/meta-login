f361725a453bdabb7d687aaee0df16046446e354
Akshat Mathur <akshat.mathur@metacube.com>
2017-01-23 12:18:47 +0530

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     # :confirmable, :lockable, :timeoutable and :omniauthable
 
     devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable, :omniauthable
+           :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable
 
     has_many :user_logins
 
